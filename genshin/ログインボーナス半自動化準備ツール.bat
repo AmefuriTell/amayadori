d49880df1a5b6f0973c08a6afc80d04fd03825fa
あめふりてる* <68197798+AmefuriTell@users.noreply.github.com>
2021-10-13 06:05:26 +0900

    原神のやつダウンロード

---
+++ genshin/ログインボーナス半自動化準備ツール.bat
@@ -0,0 +1,20 @@
+chcp 65001
+
+@echo off
+
+rem URLを入力してもらう
+
+set URL=
+set /P URL=あなたのログインボーナスページのURLを貼り付け、enterを押してください。^>
+echo %URL:&=^&amp;%
+
+rem //XMLに記入
+echo ^<?xml version="1.0" encoding="UTF-16"?^>^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>^<RegistrationInfo^>^<URI^>\OpenmiHoYoLabLogin^</URI^>^</RegistrationInfo^>^<Settings^>^<DisallowStartIfOnBatteries^>true^</DisallowStartIfOnBatteries^>^<StopIfGoingOnBatteries^>true^</StopIfGoingOnBatteries^>^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>^<StartWhenAvailable^>true^</StartWhenAvailable^>^<IdleSettings^>^<Duration^>PT10M^</Duration^>^<WaitTimeout^>PT1H^</WaitTimeout^>^<StopOnIdleEnd^>true^</StopOnIdleEnd^>^<RestartOnIdle^>false^</RestartOnIdle^>^</IdleSettings^>^</Settings^>^<Triggers^>^<CalendarTrigger^>^<StartBoundary^>1999-11-30T01:00:00^</StartBoundary^>^<ScheduleByDay^>^<DaysInterval^>1^</DaysInterval^>^</ScheduleByDay^>^</CalendarTrigger^>^</Triggers^>^<Actions Context="Author"^>^<Exec^>^<Command^>cmd^</Command^>^<Arguments^>/c start "" "%URL:&=&amp;%"^</Arguments^>^</Exec^>^</Actions^>^</Task^> > LoginSetup.xml
+
+rem タスクスケジューラに入力させる
+SCHTASKS /Create /XML LoginSetup.xml /TN ""
+
+rem XMLを削除
+
+
+pause
